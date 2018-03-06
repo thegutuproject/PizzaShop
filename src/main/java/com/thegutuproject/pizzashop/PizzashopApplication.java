697108bb2dc2306b45f8eb18a0750e6ab4b3d405
Alexandru Gutu <alex.s.gutu@gmail.com>
2018-03-06 13:56:40 -0500

    Init

---
+++ src/main/java/com/thegutuproject/pizzashop/PizzashopApplication.java
@@ -0,0 +1,12 @@
+package com.thegutuproject.pizzashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzashopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzashopApplication.class, args);
+	}
+}
