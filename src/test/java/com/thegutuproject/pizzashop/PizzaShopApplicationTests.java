cca9ba020052640791bf7b999b91978c6afebc05
Alexandru Gutu <alex.s.gutu@gmail.com>
2018-03-13 19:05:44 -0400

    Smoke testing

--- src/test/java/com/thegutuproject/pizzashop/PizzaShopApplicationTests.java
+++ src/test/java/com/thegutuproject/pizzashop/PizzaShopApplicationTests.java
@@ -1,16 +1,28 @@
 package com.thegutuproject.pizzashop;
 
+import com.thegutuproject.pizzashop.resource.OrderEntryResource;
+import com.thegutuproject.pizzashop.resource.OrderLogResource;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PizzaShopApplicationTests {
+	
+	@Autowired
+	OrderEntryResource orderEntryResource;
+	
+	@Autowired
+	OrderLogResource orderLogResource;
 
 	@Test
 	public void contextLoads() {
+		Assert.assertNotNull(orderEntryResource);
+		Assert.assertNotNull(orderLogResource);
 	}
 
 }
